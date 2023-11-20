2147ee626bac18b1d19c3676cc8ea0d6eff0c4cc
UlugbekUmirov <umirovulugbek3@gmail.com>
2023-11-20 07:50:12 +0500

    change

--- src/components/loyout/Footer.jsx
+++ src/components/loyout/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FooterStyle } from "../../styles/loyout/FooterStyle";
 
 export default function Footer() {
@@ -6,9 +7,34 @@ export default function Footer() {
       <div className="container">
         <div className="footer-items">
           <div className="protection">
-            2023 oddiy.uz <div>Barcha huquqlar himoyalangan</div>
+            2023 oddiy.uz{" "}
+            <div style={{ color: "black" }}>Barcha huquqlar himoyalangan</div>
           </div>
-          <div>Ijtimoiy tarmoqlarimiz</div>
+          <div className="socials">
+            Ijtimoiy tarmoqlarimiz
+            <ul>
+              <li>
+                <Link to="">
+                  <span className="fa fa-facebook fa-brands fa-xl"></span>
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <span className="fa-youtube fa-brands fa-xl"></span>
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <span className="fa fa-instagram fa-brands fa-xl"></span>
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <span className="fa fa-telegram fa-brands fa-xl"></span>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </FooterStyle>
--- src/styles/loyout/FooterStyle.jsx
+++ src/styles/loyout/FooterStyle.jsx
@@ -7,5 +7,52 @@ export const FooterStyle = styled.div`
   .footer-items {
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    .protection {
+      color: #3f48cc;
+      font-family: "Gilroy-Medium";
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+    .socials {
+      color: #3f48cc;
+      font-family: "Gilroy-SemiBold";
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      display: flex;
+      ul {
+        display: flex;
+        li {
+          margin: 0 14px;
+          list-style-type: none;
+          border-radius: 50%;
+          opacity: 0.2;
+          transition: all 0.1s ease;
+        }
+        li:hover {
+          opacity: 1;
+        }
+      }
+      @media (max-width: 500px) {
+        display: block;
+        ul {
+          margin-top: 20px;
+          padding: 0;
+          li:first-child {
+            margin-left: 0;
+          }
+        }
+      }
+    }
+    @media (max-width: 992px) {
+      display: block;
+      .protection {
+        margin-bottom: 20px;
+      }
+    }
   }
 `;
